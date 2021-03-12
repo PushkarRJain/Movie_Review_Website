e3ca9590709b4b68b0d917373b7bbd2b4d637257
PushkarRJain <prj20012016@gmail.com>
2021-03-12 20:55:24 +0530

    Authentication implemented

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb+srv://pj-admin:test123@cluster0.tclkd.mongodb.net/userDB", {useNewUrlParser: true});
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get("/login", function(req, res){
+  res.sendFile(__dirname+'/login.html');
+});
+app.get("/register", function(req, res){
+  res.sendFile(__dirname+'/register.html');
+});
+
+app.post("/register", function(req, res){
+  const newUser =  new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.sendFile(__dirname+'/index.html');
+    }
+  });
+
+});
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username);
+  console.log(password);
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.sendFile(__dirname+'/index.html');
+        }
+      }
+    }
+  });
+});
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+}); 
