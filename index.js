d61a3e47d240171765d9f893c62d8ef50d7a469d
PushkarRJain <prj20012016@gmail.com>
2021-03-14 22:11:19 +0530

    Final Commit

--- index.js
+++ index.js
@@ -46,43 +46,52 @@ const Review = new mongoose.model("Review", reviewSchema);
 const Avg = new mongoose.model("Avg", avgSchema);
 
 app.get("/", function(req, res){
-  var mov1 = Number(results[0].rating)/Number(results[0].tot);
+  Avg.find(function(err,results){
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+  var mov1 = Math.round(Number(results[0].rating)/Number(results[0].tot));
   if(results[0].tot==0){mov1 = 0};
   console.log(mov1);
-  var mov2 = Number(results[1].rating)/Number(results[1].tot);
+  var mov2 = Math.round(Number(results[1].rating)/Number(results[1].tot));
   if(results[1].tot==0){mov2 = 0};
   console.log(mov2);
-  var mov3 = Number(results[2].rating)/Number(results[2].tot);
+  var mov3 = Math.round(Number(results[2].rating)/Number(results[2].tot));
   if(results[2].tot==0){mov3 = 0};
   console.log(mov3);
-  var mov4 = Number(results[3].rating)/Number(results[3].tot);
+  var mov4 = Math.round(Number(results[3].rating)/Number(results[3].tot));
   if(results[3].tot==0){mov4 = 0};
   console.log(mov4);
-  var mov5 = Number(results[4].rating)/Number(results[4].tot);
+  var mov5 = Math.round(Number(results[4].rating)/Number(results[4].tot));
   if(results[4].tot==0){mov5 = 0};
   console.log(mov5);
-  var mov6 = Number(results[5].rating)/Number(results[5].tot);
+  var mov6 = Math.round(Number(results[5].rating)/Number(results[5].tot));
   if(results[5].tot==0){mov6 = 0};
   console.log(mov6);
-  var mov7 = Number(results[6].rating)/Number(results[6].tot);
+  var mov7 = Math.round(Number(results[6].rating)/Number(results[6].tot));
   if(results[6].tot==0){mov7 = 0};
   console.log(mov7);
-  var mov8 = Number(results[7].rating)/Number(results[7].tot);
+  var mov8 = Math.round(Number(results[7].rating)/Number(results[7].tot));
   if(results[7].tot==0){mov8 = 0};
   console.log(mov8);
-  var mov9 = Number(results[8].rating)/Number(results[8].tot);
+  var mov9 = Math.round(Number(results[8].rating)/Number(results[8].tot));
   if(results[8].tot==0){mov9 = 0};
   console.log(mov9);
-  var mov10 = Number(results[9].rating)/Number(results[9].tot);
+  var mov10 = Math.round(Number(results[9].rating)/Number(results[9].tot));
   if(results[9].tot==0){mov10 = 0};
   console.log(mov10);
-  var mov11 = Number(results[10].rating)/Number(results[10].tot);
+  var mov11 = Math.round(Number(results[10].rating)/Number(results[10].tot));
   if(results[10].tot==0){mov11 = 0};
   console.log(mov11);
-  var mov12 = Number(results[11].rating)/Number(results[11].tot);
+  var mov12 = Math.round(Number(results[11].rating)/Number(results[11].tot));
   if(results[11].tot==0){mov12 = 0};
   console.log(mov12);
-  res.render('index',{mov1:mov1,mov2:mov2,mov3:mov3,mov4:mov4,mov5:mov5,mov6:mov6,mov7:mov7,mov8:mov8,mov9:mov9,mov10:mov10,mov11:mov11,mov12:mov12});
+  res.render('index',{mov1:mov1,mov2:mov2,mov3:mov3,mov4:mov4,mov5:mov5,mov6:mov6,mov7:mov7,mov8:mov8,mov9:mov9,mov10:mov10,mov11:mov11,mov12:mov12,authenticate:authenticate});
+    }
+  });
 });
 app.get("/login", function(req, res){
   res.sendFile(__dirname+'/login.html');
@@ -101,18 +110,9 @@ app.get("/review", function(req, res){
     res.sendFile(__dirname+'/review.html');
   }
 });
-app.get('/trial',function(req,res){
-  Avg.find(function(err,results){
-    if(err)
-    {
-      console.log(err);
-    }
-    else
-    {
-
-      res.send("hi")
-    }
-  })
+app.get('/signout',function(req,res){
+  authenticate = 0;
+  res.redirect('/');
 })
 app.get('/:inp',function(req,res){
   var tot_rat = 0;
@@ -132,7 +132,7 @@ app.get('/:inp',function(req,res){
           tot_user = result.tot;
           console.log("rating",tot_rat);
       console.log("users",tot_user);
-      var avg = Number(tot_rat)/Number(tot_user);
+      var avg = Math.round(Number(tot_rat)/Number(tot_user));
       console.log("avg",avg);
       res.render('dummy',{results:results,avgRating:avg});
         }
@@ -160,7 +160,7 @@ app.post("/register", function(req, res){
       }
       else
       {
-        res.sendFile(__dirname+'/index.html');
+        res.redirect('/');
       }
     }
   });
@@ -184,7 +184,7 @@ app.post("/login", function(req, res){
           }
           else
           {
-            res.sendFile(__dirname+'/index.html');
+            res.redirect('/');
           }
         }
       }
